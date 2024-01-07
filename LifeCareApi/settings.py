41958277f59933806ab02190ec1e0c19e4334ccb
SoumenBaral <shumen.sb@gmail.com>
2024-01-08 02:17:46 +0600

    add some app

--- LifeCareApi/settings.py
+++ LifeCareApi/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'patient',
+    'doctor',
+    'appointment',
+    'service',
+    'contact_us',
+
+
 ]
 
 MIDDLEWARE = [
