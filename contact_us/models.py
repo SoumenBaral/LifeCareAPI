ef5c5edb49fae3f7452558487bc7c7c8c43be207
SoumenBaral <shumen.sb@gmail.com>
2024-01-08 03:51:58 +0600

    add contact us model

--- contact_us/models.py
+++ contact_us/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactUs(models.Model):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length= 12)
+    problem = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "Contact Us"
+
+    
