e59dd2f26fcdf71abe6bdd4e69b3d52b17ce7eae
SoumenBaral <shumen.sb@gmail.com>
2024-01-08 13:33:10 +0600

    add some model

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Specialization(models.Model):
+    name = models.CharField(max_length = 40)
+    slug = models.SlugField(max_length = 40)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Designation(models.Model):
+    name = models.CharField(max_length = 40)
+    slug = models.SlugField(max_length = 40)
+
+    def __str__(self) -> str:
+        return self.name
+
+class AvailableTime(models.Model):
+    name = models.CharField(max_length = 100)
+
+
+    def __str__(self) -> str:
+        return self.name
