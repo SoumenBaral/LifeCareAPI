72dadba74dce5efd0eaeec9a40fc408b2eb81851
SoumenBaral <shumen.sb@gmail.com>
2024-01-11 21:30:01 +0600

    add env

--- patient/urls.py
+++ patient/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PatientViewSet
+from .views import PatientViewSet,UserRegistrationApiView
 router = DefaultRouter() # Our Router
 router.register(r'list', PatientViewSet) # Antenna Of Router
+
 urlpatterns = [
-    path('',include(router.urls))
+    path('',include(router.urls)),
+    path('register/',UserRegistrationApiView.as_view(),name='register')
 
 ]
--- patient/views.py
+++ patient/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from .models import Patient
-from .serializers import PatientSerializer
+from .serializers import PatientSerializer,RegistrationSerializer
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
 
+class UserRegistrationApiView(APIView):
+    serializer_class  = RegistrationSerializer
+    
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save()
+            print(user)
+            return Response('done')
+        return Response(serializer.error)
