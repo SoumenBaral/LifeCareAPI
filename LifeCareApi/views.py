3932825fbf39dec0efc80725052ef3f15aefc486
SoumenBaral <shumen.sb@gmail.com>
2024-01-12 23:04:06 +0600

    done

--- LifeCareApi/views.py
+++ LifeCareApi/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home (request):
-    return HttpResponse('Welcome to Our Api')
+    return render(request,'home.html')
